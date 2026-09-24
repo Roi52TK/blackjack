bfb853bd2c1d2de43c0ec900bf98703a7b06c3fe
Roi52TK <roitheknight52@gmail.com>
2026-09-24 23:09:38 +0300

    Added UI functions contract.

---
+++ js/ui.js
@@ -0,0 +1,20 @@
+import { GAME_RESULT, CARD_SUIT } from "./constants";
+
+export class UI {
+
+    async displayNewGame(playerHand, dealerHand) {
+
+    }
+
+    async displayPlayerCard(card) {
+
+    }
+
+    async displayDealerCard(card) {
+
+    }
+
+    async displayGameOver(gameResult) {
+
+    }
+}
